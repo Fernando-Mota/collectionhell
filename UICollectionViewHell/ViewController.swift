b462d5b0ed7fdd8d7af0bb58f7f615ecb54113b9
Fernando Mota <fmaken@gmail.com>
2019-01-14 19:07:13 -0200

    Collection hell

--- UICollectionViewHell/ViewController.swift
+++ UICollectionViewHell/ViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import RxDataSources
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let array: Array<Dado> = [
+        Dado(descricao: "Item 1"),
+        Dado(descricao: "Item 2"),
+        Dado(descricao: "Item 3"),
+        Dado(descricao: "Item 4"),
+        Dado(descricao: "Item 5"),
+        Dado(descricao: "Item 6"),
+        Dado(descricao: "Item 7")
+    ]
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    let dataSource = RxCollectionViewSectionedReloadDataSource<Dados>(configureCell: {(dataSource, collectionView, indexPath, item: Dado) -> UICollectionViewCell in
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "celula", for: indexPath)
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 17))
+        label.text = item.descricao
+        cell.addSubview(label)
+        return cell
+    })
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bindViewModelToCollectionView()
     }
+    
+    private func bindViewModelToCollectionView(){
+        dataSource.configureSupplementaryView = {(dataSource, collectionView, kind, indexPath) -> UICollectionReusableView in
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "Header", for: indexPath)
+            header.frame.size.height = 500
+            return header
+        }
+        
+        dataSource.configureSupplementaryView = {(dataSource, collectionView, kind, indexPath) -> UICollectionReusableView in
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "Footer", for: indexPath)
+            return footer
+        }
+        
+        let items = Observable.just([Dados(descricoes: array)])
+        
+        items.bind(to: collectionView.rx.items(dataSource: dataSource)).disposed(by: disposeBag)
+    }
+    
+
+}
+
+struct Dado {
+    
+    let descricao: String
+    
+}
+
+struct Dados {
+    
+    var descricoes: [Dado]
+    
+}
 
 
+extension Dados: SectionModelType {
+    
+    typealias Item = Dado
+    
+    var items: [Dado] {
+        return descricoes
+    }
+    
+    init(original: Dados, items: [Dado]) {
+        self = original
+        descricoes = items
+    }
 }
 
